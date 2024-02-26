00bf579b1c241e4c26324d7d47ea59d87c0354db
DonVladon96 <Petrov.Vladik2010@yandex.com>
2024-02-26 19:39:43 +0400

    refactoring main file, add new comments

--- js/main.js
+++ js/main.js
@@ -8,28 +8,23 @@ const help = document.querySelector(".help");
 const guessBtn = document.querySelector(".guess-button");
 const guessField = document.querySelector(".guessField");
 const resetGameBtn = document.querySelector(".reset-button");
-
 // Выбор диапазона
 const modal = document.querySelector("#modal");
 // Подтверждение выбора
 const confirmBtn = document.querySelector(".confirm");
 // Подсказка какой диапазон был выбран
 const selectedRenge = document.querySelector(".selected-renge");
-
 const minNumber = document.querySelector("#startNumber");
 const maxNumber = document.querySelector("#endNumber");
-
 let min = 0;
 let max = 100;
 let guessCount = 1;
 let userGuess;
 let randomNumber;
-
 // Получаем загаданное число
 const getRandomNumber = () => {
   randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 };
-
 // Функция проверки введенного числа с загаданным
 const checkGuess = () => {
   resultBox.style.display = "flex";
@@ -37,16 +32,15 @@ const checkGuess = () => {
   lowOrHi.style.display = "block";
   guesses.style.display = "block";
   guesses.textContent += userGuess + " ";
-
   if (userGuess === randomNumber) {
-    lastResult.textContent = `Поздравляю! Ты угадал число. Количество попыток: ${guessCount}`;
+    lastResult.textContent =  `Поздравляю! Ты угадал число. Количество попыток: ${guessCount}` ;
     lastResult.style.backgroundColor = "green";
     help.style.display = "none";
     guesses.style.display = "none";
     lowOrHi.style.display = "none";
     gameOver();
   } else {
-    lastResult.textContent = `Неверно! Попыток: ${guessCount}`;
+    lastResult.textContent =  `Неверно! Попыток: ${guessCount}` ;
     lastResult.style.backgroundColor = "rgba(235, 63, 63, 0.602)";
     // Отображение подсказки при 3х неверных ответах
     if (guessCount === 3) {
@@ -69,12 +63,10 @@ const checkGuess = () => {
       lowOrHi.classList.add("warning");
     }
   }
-
   guessCount++;
   guessField.value = "";
   guessField.focus();
 };
-
 // Завершение игры
 const gameOver = () => {
   guessField.disabled = true;
@@ -82,7 +74,6 @@ const gameOver = () => {
   resetGameBtn.style.display = "block";
   resetGameBtn.addEventListener("click", resetGame);
 };
-
 // Очистка значений и запуск новой игры
 const resetGame = () => {
   guessCount = 1;
@@ -93,29 +84,22 @@ const resetGame = () => {
   guessBtn.disabled = false;
   guessField.value = "";
   guessField.focus();
-
   getRandomNumber();
 };
-
 // Отображение модального окна
 const initialize = () => {
   modal.classList.add("shown");
 };
-
 guessBtn.addEventListener("click", checkGuess);
-
 confirmBtn.addEventListener("click", () => {
   modal.classList.remove("shown");
   selectedRenge.textContent += min + "-" + max;
   getRandomNumber();
 });
-
 minNumber.addEventListener("change", () => {
   min = Number(minNumber.value);
 });
-
 maxNumber.addEventListener("change", () => {
   max = Number(maxNumber.value);
 });
-
 initialize();
